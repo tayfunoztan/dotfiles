4b988e1d822692db0ce1d7c340b2d5be3161f541
Tayfun Oztan <tayfunn.oztann@gmail.com>
2021-02-05 18:02:02 +0300

    fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+
+# git prompt settings
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_char_dirtystate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_conflictedstate "+"
+set -g __fish_git_prompt_color_dirtystate yellow
+set -g __fish_git_prompt_color_cleanstate green --bold
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_branch cyan --dim --italics
+
+# No greeting.
+set fish_greeting
+
+# don't describe the command for darwin
+# https://github.com/fish-shell/fish-shell/issues/6270
+function __fish_describe_command; end
