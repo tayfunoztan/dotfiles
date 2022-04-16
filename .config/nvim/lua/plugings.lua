dc1d45f5b187643b0479e2f72f1c57bc4181efae
Tayfun Oztan <tayfunn.oztann@gmail.com>
2022-04-16 05:46:55 +0300

    wip neovim

--- .config/nvim/lua/plugings.lua
+++ .config/nvim/lua/plugings.lua
@@ -1,260 +1,270 @@
-local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+  vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 
-vim.cmd(
-    [[
+vim.cmd([[
   augroup packer_user_config
   autocmd!
   autocmd BufWritePost plugins.lua source <afile> | PackerCompile
   augroup end
-]]
-)
+]])
 
-return require("packer").startup {
-    function(use)
-        use "wbthomason/packer.nvim"
+pcall(require, "impatient")
 
-        use "lewis6991/impatient.nvim"
-        use {
-            "dstein64/vim-startuptime",
-            cmd = "StartupTime",
-            config = function()
-                vim.g.startuptime_tries = 15
-                vim.g.startuptime_exe_args = {"+let g:auto_session_enabled = 0"}
-            end
-        }
+return require("packer").startup({
+  function(use)
+    use("wbthomason/packer.nvim")
 
-        use "nvim-lua/plenary.nvim"
-        use "nvim-lua/popup.nvim"
+    use("lewis6991/impatient.nvim")
+    use({
+      "dstein64/vim-startuptime",
+      cmd = "StartupTime",
+      config = function()
+        vim.g.startuptime_tries = 15
+        vim.g.startuptime_exe_args = { "+let g:auto_session_enabled = 0" }
+      end,
+    })
 
-        use {
-            "tpope/vim-fugitive",
-            cmd = {"Git", "Gvsplit", "Gdiffsplit", "Gvdiffsplit"}
-        }
-        use "tpope/vim-rhubarb"
-        use "tpope/vim-surround"
-        use "tpope/vim-repeat"
-        use "tpope/vim-commentary"
+    use("nvim-lua/plenary.nvim")
+    use("nvim-lua/popup.nvim")
 
-        use {"junegunn/fzf", run = "./install --all"}
-        use {"junegunn/fzf.vim"}
-        -- use 'junegunn/vim-peekaboo'
+    use({
+      "tpope/vim-fugitive",
+      cmd = { "Git", "Gvsplit", "Gdiffsplit", "Gvdiffsplit" },
+    })
+    use("tpope/vim-rhubarb")
+    use("tpope/vim-surround")
+    use("tpope/vim-repeat")
+    use("tpope/vim-commentary")
 
-        use {
-            "TimUntersberger/neogit",
-            cmd = "Neogit",
-            config = function()
-                require("config.neogit")
-            end
-        }
+    use({ "junegunn/fzf", run = "./install --all" })
+    use({ "junegunn/fzf.vim" })
+    -- use 'junegunn/vim-peekaboo'
 
-        use {
-            "sindrets/diffview.nvim",
-            cmd = {"DiffviewOpen", "DiffviewFileHistory", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles"},
-            module = "diffview",
-            config = function()
-                require("config.diffview")
-            end
-        }
+    use({
+      "TimUntersberger/neogit",
+      cmd = "Neogit",
+      config = function()
+        require("config.neogit")
+      end,
+    })
 
-        use {
-            "lewis6991/gitsigns.nvim",
-            event = "BufReadPre",
-            requires = {"nvim-lua/plenary.nvim"},
-            config = function()
-                require("config.gitsigns")
-            end
-        }
+    use({
+      "sindrets/diffview.nvim",
+      cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+      module = "diffview",
+      config = function()
+        require("config.diffview")
+      end,
+    })
 
-        use {
-            "nvim-telescope/telescope.nvim",
-            cmd = {"Telescope"},
-            keys = {"<leader>ff", "<leader>fo", "<leader>fg", "<leader>fb"},
-            requires = {
-                "nvim-lua/popup.nvim",
-                "nvim-lua/plenary.nvim",
-                {"nvim-telescope/telescope-fzf-native.nvim", run = "make"},
-                {"nvim-telescope/telescope-file-browser.nvim"}
-            },
-            config = function()
-                require("config.telescope")
-            end
-        }
+    use({
+      "lewis6991/gitsigns.nvim",
+      event = "BufReadPre",
+      requires = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("config.gitsigns")
+      end,
+    })
 
-        -----------------------------------------------------------------------------//
-        -- LSP,Completion & Debugger {{{1
-        -----------------------------------------------------------------------------//
-        use {
-            "neovim/nvim-lspconfig",
-            config = function()
-                require("config.lspconfig")
-            end,
-            requires = {
-                "williamboman/nvim-lsp-installer",
-                "jose-elias-alvarez/null-ls.nvim"
-            }
-        }
-        use {
-            "hrsh7th/nvim-cmp",
-            config = function()
-                require("config.nvim-cmp")
-            end,
-            -- event = "InsertEnter",
-            requires = {
-                -- { "hrsh7th/cmp-nvim-lsp", after = "nvim-lspconfig" },
-                {"hrsh7th/cmp-nvim-lsp"},
-                {"hrsh7th/cmp-nvim-lsp-document-symbol", after = "nvim-cmp"},
-                {"hrsh7th/cmp-path", after = "nvim-cmp"},
-                {"hrsh7th/cmp-buffer", after = "nvim-cmp"},
-                {"hrsh7th/cmp-vsnip", after = "nvim-cmp"},
-                {"hrsh7th/vim-vsnip", after = "nvim-cmp"},
-                {
-                    "L3MON4D3/LuaSnip",
-                    wants = "friendly-snippets",
-                    config = function()
-                        require("config.luasnip")
-                    end
-                }
-            }
-        }
+    use({
+      "nvim-telescope/telescope.nvim",
+      cmd = { "Telescope" },
+      keys = { "<leader>ff", "<leader>fo", "<leader>fg", "<leader>fb" },
+      requires = {
+        "nvim-lua/popup.nvim",
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+        { "nvim-telescope/telescope-file-browser.nvim" },
+      },
+      config = function()
+        require("config.telescope")
+      end,
+    })
 
-        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+    use({
+      "neovim/nvim-lspconfig",
+      config = function()
+        require("config.lspconfig")
+      end,
+      requires = {
+        "williamboman/nvim-lsp-installer",
+        "jose-elias-alvarez/null-ls.nvim",
+      },
+    })
+    use({
+      "hrsh7th/nvim-cmp",
+      config = function()
+        require("config.nvim-cmp")
+      end,
+      -- event = "InsertEnter",
+      requires = {
+        -- { "hrsh7th/cmp-nvim-lsp", after = "nvim-lspconfig" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-nvim-lsp-document-symbol", after = "nvim-cmp" },
+        { "hrsh7th/cmp-path", after = "nvim-cmp" },
+        { "hrsh7th/cmp-buffer", after = "nvim-cmp" },
+        { "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" },
+        {
+          "L3MON4D3/LuaSnip",
+          requires = "rafamadriz/friendly-snippets",
+          config = function()
+            require("config.luasnip")
+          end,
+        },
+      },
+    })
 
-        use "christoomey/vim-tmux-navigator"
-        use "tayfunoztan/ReplaceWithRegister"
-        use "Raimondi/delimitMate"
-        use {
-            "AndrewRadev/splitjoin.vim",
-            keys = {"gJ", "gS"}
-        }
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
-        use {
-            "fatih/vim-go",
-            run = ":GoUpdateBinaries",
-            setup = function()
-                vim.g.go_code_completion_enabled = 0
-                vim.g.go_def_mapping_enabled = 0
-                vim.g.go_echo_go_info = 0
-                vim.g.gopls_enabled = 0
-                vim.g.go_doc_keywordprg_enabled = 0
+    use("christoomey/vim-tmux-navigator")
+    use("tayfunoztan/ReplaceWithRegister")
+    use("Raimondi/delimitMate")
+    use({
+      "AndrewRadev/splitjoin.vim",
+      keys = { "gJ", "gS" },
+    })
 
-                -- vim.g.go_imports_mode = "gopls"
-                -- vim.g.go_imports_autosave = 1
-                -- vim.g.go_gopls_complete_unimported = 1
-                -- vim.g.go_fmt_command = "goimports"
+    use({
+      "fatih/vim-go",
+      run = ":GoUpdateBinaries",
+      setup = function()
+        vim.g.go_code_completion_enabled = 0
+        vim.g.go_def_mapping_enabled = 0
+        vim.g.go_echo_go_info = 0
+        vim.g.gopls_enabled = 0
+        vim.g.go_doc_keywordprg_enabled = 0
 
-                vim.g.go_test_show_name = 0
-                vim.g.go_list_type = "quickfix"
+        -- vim.g.go_imports_mode = "gopls"
+        -- vim.g.go_imports_autosave = 1
+        -- vim.g.go_gopls_complete_unimported = 1
+        -- vim.g.go_fmt_command = "goimports"
 
-                vim.g.go_highlight_build_constraints = 1
-                vim.g.go_highlight_operators = 1
-                vim.g.go_highlight_extra_types = 0
-                vim.g.go_highlight_functions = 1
-                vim.g.go_highlight_function_parameters = 0
-                vim.g.go_highlight_function_calls = 1
-                vim.g.go_highlight_types = 1
-                vim.g.go_highlight_fields = 1
-            end
-        }
+        vim.g.go_test_show_name = 0
+        vim.g.go_list_type = "quickfix"
 
-        use "folke/lua-dev.nvim"
+        vim.g.go_highlight_build_constraints = 1
+        vim.g.go_highlight_operators = 1
+        vim.g.go_highlight_extra_types = 0
+        vim.g.go_highlight_functions = 1
+        vim.g.go_highlight_function_parameters = 0
+        vim.g.go_highlight_function_calls = 1
+        vim.g.go_highlight_types = 1
+        vim.g.go_highlight_fields = 1
+      end,
+    })
 
-        use {
-            "simrat39/rust-tools.nvim",
-            config = function()
-                require("rust-tools").setup({})
-            end
-        }
-        use "othree/yajs.vim"
-        use "MaxMEllon/vim-jsx-pretty"
-        use {"iamcco/markdown-preview.nvim", ft = "markdown", run = "cd app && yarn install"}
-        use "dag/vim-fish"
+    use("folke/lua-dev.nvim")
 
-        use {
-            "folke/which-key.nvim",
-            config = function()
-                require("which-key").setup {}
-            end
-        }
+    use({
+      "simrat39/rust-tools.nvim",
+      config = function()
+        require("rust-tools").setup({})
+      end,
+    })
+    use("othree/yajs.vim")
+    use("MaxMEllon/vim-jsx-pretty")
+    use({ "iamcco/markdown-preview.nvim", ft = "markdown", run = "cd app && yarn install" })
+    use("dag/vim-fish")
 
-        use {
-            "nvim-lualine/lualine.nvim",
-            config = function()
-                require("config.lualine")
-            end
-        }
+    use({
+      "folke/which-key.nvim",
+      config = function()
+        require("which-key").setup({})
+      end,
+    })
 
-        use {
-            "karb94/neoscroll.nvim",
-            config = function()
-                require("neoscroll").setup {
-                    mappings = {"<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-e>", "<C-y>", "zt", "zz", "zb"},
-                    stop_eof = false,
-                    hide_cursor = true
-                }
-            end
-        }
+    use({
+      "nvim-lualine/lualine.nvim",
+      config = function()
+        require("config.lualine")
+      end,
+    })
 
-        -- use {
-        --   'petertriho/nvim-scrollbar',
-        --   config = function()
-        --     require('scrollbar').setup {
-        --       -- handle = {
-        --       --   color = require('as.highlights').get_hl('PmenuSbar', 'bg'),
-        --       -- },
-        --       -- NOTE: If telescope is not explicitly excluded this garbles input into its prompt buffer
-        --       excluded_buftypes = { 'nofile', 'terminal', 'prompt' },
-        --       excluded_filetypes = { 'packer', 'TelescopePrompt', 'NvimTree' },
-        --     }
-        --   end,
-        -- }
+    use({
+      "karb94/neoscroll.nvim",
+      config = function()
+        require("neoscroll").setup({
+          mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-e>", "<C-y>", "zt", "zz", "zb" },
+          stop_eof = false,
+          hide_cursor = true,
+        })
+      end,
+    })
 
-        -- use {
-        --   'akinsho/nvim-bufferline.lua',
-        --   requires = 'kyazdani42/nvim-web-devicons',
-        --   event = "BufReadPre",
-        --   config = function()
-        --     require('config.bufferline')
-        --   end,
-        -- }
+    -- use {
+    --   'petertriho/nvim-scrollbar',
+    --   config = function()
+    --     require('scrollbar').setup {
+    --       -- handle = {
+    --       --   color = require('as.highlights').get_hl('PmenuSbar', 'bg'),
+    --       -- },
+    --       -- NOTE: If telescope is not explicitly excluded this garbles input into its prompt buffer
+    --       excluded_buftypes = { 'nofile', 'terminal', 'prompt' },
+    --       excluded_filetypes = { 'packer', 'TelescopePrompt', 'NvimTree' },
+    --     }
+    --   end,
+    -- }
 
-        use {
-            "folke/trouble.nvim",
-            requires = "kyazdani42/nvim-web-devicons",
-            config = function()
-                require("trouble").setup {}
-            end
-        }
+    -- use {
+    --   'akinsho/nvim-bufferline.lua',
+    --   requires = 'kyazdani42/nvim-web-devicons',
+    --   event = "BufReadPre",
+    --   config = function()
+    --     require('config.bufferline')
+    --   end,
+    -- }
 
-        use {
-            "kyazdani42/nvim-web-devicons",
-            config = function()
-                require("nvim-web-devicons").setup({default = true})
-            end
-        }
+    use({
+      "folke/trouble.nvim",
+      requires = "kyazdani42/nvim-web-devicons",
+      config = function()
+        require("trouble").setup({})
+      end,
+    })
 
-        use "t9md/vim-choosewin"
-        use {
-            "kyazdani42/nvim-tree.lua",
-            config = function()
-                require("config.nvim-tree")
-            end
-        }
-        use {
-            "j-hui/fidget.nvim",
-            config = function()
-                require("fidget").setup()
-            end
-        }
-        -- use 'arkav/lualine-lsp-progress'
+    use({
+      "kyazdani42/nvim-web-devicons",
+      config = function()
+        require("nvim-web-devicons").setup({ default = true })
+      end,
+    })
 
-        use "gruvbox-community/gruvbox"
-        use "folke/tokyonight.nvim"
-        use "~/dev/seoul256.nvim"
-    end
-}
+    use("t9md/vim-choosewin")
+    -- use {
+    --     "kyazdani42/nvim-tree.lua",
+    --     config = function()
+    --         require("config.nvim-tree")
+    --     end
+    -- }
+    use({
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v2.x",
+      keys = "<leader>n",
+      cmd = "NeoTree",
+      config = function()
+        require("config.neo-tree")
+      end,
+      requires = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "kyazdani42/nvim-web-devicons",
+      },
+    })
+    use({
+      "j-hui/fidget.nvim",
+      config = function()
+        require("fidget").setup()
+      end,
+    })
+    -- use 'arkav/lualine-lsp-progress'
+
+    use("gruvbox-community/gruvbox")
+    use("folke/tokyonight.nvim")
+    use("~/dev/seoul256.nvim")
+  end,
+})
 
 -- vim:foldmethod=marker
